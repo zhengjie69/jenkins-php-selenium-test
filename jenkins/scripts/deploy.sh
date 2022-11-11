611c104e13463c7d568adda1d285efba6323d0a8
Lim Zheng Jie <zhengjie69@hotmail.com>
2022-11-11 15:20:38 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,6 +5,11 @@ docker run -d -p 80:80 --name my-apache-php-app -v C:/Users/thezh/OneDrive/Deskt
 sleep 1
 set +x
 
+set -x
+docker run -d -p 80:80 --name my-apache-php-app -v C:/Users/thezh/OneDrive/Desktop/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+sleep 1
+set +x
+
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
 
